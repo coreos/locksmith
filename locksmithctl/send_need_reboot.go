73498ac1698fb77fc793b9f9b6269a6e77a23356
Brandon Philips <brandon@ifup.co>
2014-05-02 12:40:57 -0700

    feat(locksmithctl): add send-need-reboot
    introduce a tool to send a need reboot signal via dbus.

---
+++ locksmithctl/send_need_reboot.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/coreos/locksmith/third_party/github.com/godbus/dbus"
+)
+
+var (
+	cmdSendNeedReboot = &Command{
+		Name:        "send-need-reboot",
+		Summary:     "send a 'need reboot' signal over dbus.",
+		Description: `Send a fake 'need reboot' signal to test locksmithd
+without a full update cycle via update engine.`,
+		Run:         runSendNeedReboot,
+	}
+)
+
+func runSendNeedReboot(args []string) int {
+	conn, err := dbus.SystemBusPrivate()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error connecting to system dbus:", err)
+		return 1
+	}
+
+	methods := []dbus.Auth{dbus.AuthExternal(strconv.Itoa(os.Getuid()))}
+	err = conn.Auth(methods)
+	if err != nil {
+		conn.Close()
+		fmt.Fprintln(os.Stderr, "error authing to system dbus:", err)
+		return 1
+	}
+
+	err = conn.Hello()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error sending hello:", err)
+		conn.Close()
+		return 1
+	}
+
+	err = conn.Emit("/com/coreos/update1",
+		"com.coreos.update1.Manager.StatusUpdate",
+		int64(0), float64(0),
+		string("UPDATE_STATUS_UPDATED_NEED_REBOOT"),
+		string(""), int64(0))
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error emitting signal:", err)
+		conn.Close()
+		return 1
+	}
+
+	// TODO(philips): figure out a way to make conn.Close() block until
+	// everything is flushed.
+	time.Sleep(time.Second)
+	conn.Close()
+
+	return 0
+}
