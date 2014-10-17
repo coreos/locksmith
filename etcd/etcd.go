coreos/locksmith

commit a621605a94055a2e8ef3ebcdde85b277fd564adf

locksmithctl: add TLS support

etcd/etcd.go
@@ -1,0 +1,44 @@
+package etcd
+
+import (
+	"fmt"
+
+	"github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
+)
+
+// EtcdClient is a simple wrapper around the go-etcd client to facilitate testing
+type EtcdClient interface {
+	Create(key string, value string, ttl uint64) (*etcd.Response, error)
+	CompareAndSwap(key string, value string, ttl uint64, prevValue string, prevIndex uint64) (*etcd.Response, error)
+	Get(key string, sort, recursive bool) (*etcd.Response, error)
+}
+
+type Error struct {
+	ErrorCode int    `json:"errorCode"`
+	Message   string `json:"message"`
+	Cause     string `json:"cause"`
+	Index     uint64 `json:"index"`
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("%v: %v (%v) [%v]", e.ErrorCode, e.Message, e.Cause, e.Index)
+}
+
+type TLSConfig struct {
+	CertFile string
+	KeyFile  string
+	CAFile   string
+}
+
+const (
+	ErrorKeyNotFound = 100
+	ErrorNodeExist   = 105
+)
+
+// NewClient creates a new EtcdClient
+func NewClient(machines []string, tc *TLSConfig) (EtcdClient, error) {
+	if tc != nil {
+		return etcd.NewTLSClient(machines, tc.CertFile, tc.KeyFile, tc.CAFile)
+	}
+	return etcd.NewClient(machines), nil
+}

lock/etcd.go
@@ -4,8 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 
-	etcdError "github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/etcd/error"
-	"github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
+	"github.com/coreos/locksmith/etcd"
 )
 
 const (
@@ -14,26 +13,17 @@ const (
 	SemaphorePrefix = keyPrefix + "/semaphore"
 )
 
-// etcdInterface is a simple wrapper around the go-etcd client to facilitate testing
-type etcdInterface interface {
-	Create(key string, value string, ttl uint64) (*etcd.Response, error)
-	CompareAndSwap(key string, value string, ttl uint64, prevValue string, prevIndex uint64) (*etcd.Response, error)
-	Get(key string, sort, recursive bool) (*etcd.Response, error)
-}
-
-// EtcdLockClient is a wrapper around the go-etcd client that provides
+// EtcdLockClient is a wrapper around the etcd client that provides
 // simple primitives to operate on the internal semaphore and holders
 // structs through etcd.
 type EtcdLockClient struct {
-	client etcdInterface
+	client etcd.EtcdClient
 }
 
-func NewEtcdLockClient(machines []string) (client *EtcdLockClient, err error) {
-	ec := etcd.NewClient(machines)
+func NewEtcdLockClient(ec etcd.EtcdClient) (client *EtcdLockClient, err error) {
 	client = &EtcdLockClient{ec}
 	err = client.Init()
-
-	return client, err
+	return
 }
 
 // Init sets an initial copy of the semaphore if it doesn't exist yet.
@@ -46,8 +36,8 @@ func (c *EtcdLockClient) Init() (err error) {
 
 	_, err = c.client.Create(SemaphorePrefix, string(b), 0)
 	if err != nil {
-		eerr, ok := err.(*etcd.EtcdError)
-		if ok && eerr.ErrorCode == etcdError.EcodeNodeExist {
+		eerr, ok := err.(*etcd.Error)
+		if ok && eerr.ErrorCode == etcd.ErrorNodeExist {
 			return nil
 		}
 	}

lock/etcd_test.go
@@ -5,24 +5,24 @@ import (
 	"reflect"
 	"testing"
 
-	etcdError "github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/etcd/error"
-	"github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
+	goetcd "github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
+	"github.com/coreos/locksmith/etcd"
 )
 
 type testEtcdClient struct {
 	err  error
-	resp *etcd.Response
+	resp *goetcd.Response
 }
 
-func (t *testEtcdClient) Create(key string, value string, ttl uint64) (*etcd.Response, error) {
+func (t *testEtcdClient) Create(key string, value string, ttl uint64) (*goetcd.Response, error) {
 	return t.resp, t.err
 }
 
-func (t *testEtcdClient) CompareAndSwap(key string, value string, ttl uint64, prevValue string, prevIndex uint64) (*etcd.Response, error) {
+func (t *testEtcdClient) CompareAndSwap(key string, value string, ttl uint64, prevValue string, prevIndex uint64) (*goetcd.Response, error) {
 	return t.resp, t.err
 }
 
-func (t *testEtcdClient) Get(key string, sort, recursive bool) (*etcd.Response, error) {
+func (t *testEtcdClient) Get(key string, sort, recursive bool) (*goetcd.Response, error) {
 	return t.resp, t.err
 }
 
@@ -32,8 +32,8 @@ func TestEtcdLockClientInit(t *testing.T) {
 		want bool
 	}{
 		{nil, false},
-		{&etcd.EtcdError{ErrorCode: etcdError.EcodeNodeExist}, false},
-		{&etcd.EtcdError{ErrorCode: etcdError.EcodeKeyNotFound}, true},
+		{&etcd.Error{ErrorCode: etcd.ErrorNodeExist}, false},
+		{&etcd.Error{ErrorCode: etcd.ErrorKeyNotFound}, true},
 		{errors.New("some random error"), true},
 	} {
 		elc := &EtcdLockClient{
@@ -46,9 +46,9 @@ func TestEtcdLockClientInit(t *testing.T) {
 	}
 }
 
-func makeResponse(idx int, val string) *etcd.Response {
-	return &etcd.Response{
-		Node: &etcd.Node{
+func makeResponse(idx int, val string) *goetcd.Response {
+	return &goetcd.Response{
+		Node: &goetcd.Node{
 			Value:         val,
 			ModifiedIndex: uint64(idx),
 		},
@@ -58,13 +58,13 @@ func makeResponse(idx int, val string) *etcd.Response {
 func TestEtcdLockClientGet(t *testing.T) {
 	for i, tt := range []struct {
 		ee error
-		er *etcd.Response
+		er *goetcd.Response
 		ws *Semaphore
 		we bool
 	}{
 		// errors returned from etcd
 		{errors.New("some error"), nil, nil, true},
-		{&etcd.EtcdError{ErrorCode: etcdError.EcodeKeyNotFound}, nil, nil, true},
+		{&etcd.Error{ErrorCode: etcd.ErrorKeyNotFound}, nil, nil, true},
 		// bad JSON should cause errors
 		{nil, makeResponse(0, "asdf"), nil, true},
 		{nil, makeResponse(0, `{"semaphore:`), nil, true},
@@ -108,8 +108,8 @@ func TestEtcdLockClientSet(t *testing.T) {
 		{&Semaphore{}, nil, false},
 		{&Semaphore{Index: uint64(1234)}, nil, false},
 		// all errors returned from etcd should propagate
-		{&Semaphore{}, &etcd.EtcdError{ErrorCode: etcdError.EcodeNodeExist}, true},
-		{&Semaphore{}, &etcd.EtcdError{ErrorCode: etcdError.EcodeKeyNotFound}, true},
+		{&Semaphore{}, &etcd.Error{ErrorCode: etcd.ErrorNodeExist}, true},
+		{&Semaphore{}, &etcd.Error{ErrorCode: etcd.ErrorKeyNotFound}, true},
 		{&Semaphore{}, errors.New("some random error"), true},
 	} {
 		elc := &EtcdLockClient{

locksmithctl/daemon.go
@@ -118,7 +118,7 @@ func (r rebooter) lockAndReboot(lck *lock.Lock) {
 }
 
 func setupLock() (lck *lock.Lock, err error) {
-	elc, err := lock.NewEtcdLockClient([]string{globalFlags.Endpoint})
+	elc, err := getClient()
 	if err != nil {
 		return nil, fmt.Errorf("Error initializing etcd client: %v", err)
 	}

locksmithctl/lock.go
@@ -20,7 +20,7 @@ machine-id. Under normal operation this should not be necessary.`,
 )
 
 func runLock(args []string) (exit int) {
-	elc, err := lock.NewEtcdLockClient([]string{globalFlags.Endpoint})
+	elc, err := getClient()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error initializing etcd client:", err)
 		return 1

locksmithctl/locksmithctl.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"path"
 	"text/tabwriter"
+
+	"github.com/coreos/locksmith/etcd"
+	"github.com/coreos/locksmith/lock"
 )
 
 const (
@@ -20,8 +23,11 @@ var (
 	globalFlagset *flag.FlagSet = flag.NewFlagSet("locksmithctl", flag.ExitOnError)
 
 	globalFlags = struct {
-		Debug    bool
-		Endpoint string
+		Debug        bool
+		Endpoint     string
+		EtcdKeyFile  string
+		EtcdCertFile string
+		EtcdCAFile   string
 	}{}
 )
 
@@ -31,6 +37,9 @@ func init() {
 
 	globalFlagset.BoolVar(&globalFlags.Debug, "debug", false, "Print out debug information to stderr.")
 	globalFlagset.StringVar(&globalFlags.Endpoint, "endpoint", "http://127.0.0.1:4001", "etcd endpoint for locksmith. Defaults to the local instance.")
+	globalFlagset.StringVar(&globalFlags.EtcdKeyFile, "etcd-keyfile", "", "etcd key file authentication")
+	globalFlagset.StringVar(&globalFlags.EtcdCertFile, "etcd-certfile", "", "etcd cert file authentication")
+	globalFlagset.StringVar(&globalFlags.EtcdCAFile, "etcd-cafile", "", "etcd CA file authentication")
 
 	commands = []*Command{
 		cmdHelp,
@@ -99,4 +108,26 @@ func main() {
 	}
 
 	os.Exit(cmd.Run(cmd.Flags.Args()))
+}
+
+// getLockClient returns an initialized EtcdLockClient, using an etcd
+// client configured from the global etcd flags
+func getClient() (*lock.EtcdLockClient, error) {
+	var tc *etcd.TLSConfig
+	if globalFlags.EtcdCAFile != "" || globalFlags.EtcdCertFile != "" || globalFlags.EtcdKeyFile != "" {
+		tc = &etcd.TLSConfig{
+			CertFile: globalFlags.EtcdCertFile,
+			KeyFile:  globalFlags.EtcdKeyFile,
+			CAFile:   globalFlags.EtcdCAFile,
+		}
+	}
+	ec, err := etcd.NewClient([]string{globalFlags.Endpoint}, tc)
+	if err != nil {
+		return nil, err
+	}
+	lc, err := lock.NewEtcdLockClient(ec)
+	if err != nil {
+		return nil, err
+	}
+	return lc, err
 }

locksmithctl/reboot.go
@@ -24,7 +24,7 @@ func runReboot(args []string) int {
 		return 1
 	}
 
-	elc, err := lock.NewEtcdLockClient([]string{globalFlags.Endpoint})
+	elc, err := getClient()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error initializing etcd client:", err)
 		return 1

locksmithctl/set_max.go
@@ -25,7 +25,7 @@ func runSetMax(args []string) (exit int) {
 		return 1
 	}
 
-	elc, err := lock.NewEtcdLockClient([]string{globalFlags.Endpoint})
+	elc, err := getClient()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error initializing etcd client:", err)
 		return 1

commit 285344035d785636477ffce1f78b294a7ca382f2

lock: fix etcd error handling
The type conversion with the new locksmith/etcd.Error introduced in
a621605a94055a2e8ef3ebcdde85b277fd564adf unfortunately does not work,
since the underlying type is still from the go-etcd library in this
case. Ideally go-etcd should not be exposed anywhere except
locksmith/etcd so I went down a path of wrapping every go-etcd call and
converting go-etcd.EtcdError to etcd.Error, but this turned into a
rabbit hole (since it implies that go-etcd.Response should also be
converted, which necessitates locksmith/etcd.Response, ...)

For now, revert to just leaking the go-etcd types into the lock package.
To be revisited....

etcd/etcd.go
@@ -1,27 +1,13 @@
 package etcd
 
-import (
-	"fmt"
+import "github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
 
-	"github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
-)
-
-// EtcdClient is a simple wrapper around the go-etcd client to facilitate testing
+// EtcdClient is a simple wrapper around the underlying etcd client to facilitate
+// testing and minimize the interface between locksmith and the actual etcd client
 type EtcdClient interface {
 	Create(key string, value string, ttl uint64) (*etcd.Response, error)
 	CompareAndSwap(key string, value string, ttl uint64, prevValue string, prevIndex uint64) (*etcd.Response, error)
 	Get(key string, sort, recursive bool) (*etcd.Response, error)
-}
-
-type Error struct {
-	ErrorCode int    `json:"errorCode"`
-	Message   string `json:"message"`
-	Cause     string `json:"cause"`
-	Index     uint64 `json:"index"`
-}
-
-func (e Error) Error() string {
-	return fmt.Sprintf("%v: %v (%v) [%v]", e.ErrorCode, e.Message, e.Cause, e.Index)
 }
 
 type TLSInfo struct {

lock/etcd.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 
+	// TODO(jonboulle): this is a leaky abstraction, but we don't want to reimplement all go-etcd types in locksmith/etcd. This should go away once go-etcd is replaced.
+	goetcd "github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
 	"github.com/coreos/locksmith/etcd"
 )
 
@@ -36,7 +38,7 @@ func (c *EtcdLockClient) Init() (err error) {
 
 	_, err = c.client.Create(SemaphorePrefix, string(b), 0)
 	if err != nil {
-		eerr, ok := err.(*etcd.Error)
+		eerr, ok := err.(*goetcd.EtcdError)
 		if ok && eerr.ErrorCode == etcd.ErrorNodeExist {
 			return nil
 		}

lock/etcd_test.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"testing"
 
+	// TODO(jonboulle): this is a leaky abstraction, but we don't want to reimplement all go-etcd types in locksmith/etcd. This should go away once go-etcd is replaced.
 	goetcd "github.com/coreos/locksmith/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
 	"github.com/coreos/locksmith/etcd"
 )
@@ -32,8 +33,8 @@ func TestEtcdLockClientInit(t *testing.T) {
 		want bool
 	}{
 		{nil, false},
-		{&etcd.Error{ErrorCode: etcd.ErrorNodeExist}, false},
-		{&etcd.Error{ErrorCode: etcd.ErrorKeyNotFound}, true},
+		{&goetcd.EtcdError{ErrorCode: etcd.ErrorNodeExist}, false},
+		{&goetcd.EtcdError{ErrorCode: etcd.ErrorKeyNotFound}, true},
 		{errors.New("some random error"), true},
 	} {
 		elc := &EtcdLockClient{
@@ -64,7 +65,7 @@ func TestEtcdLockClientGet(t *testing.T) {
 	}{
 		// errors returned from etcd
 		{errors.New("some error"), nil, nil, true},
-		{&etcd.Error{ErrorCode: etcd.ErrorKeyNotFound}, nil, nil, true},
+		{&goetcd.EtcdError{ErrorCode: etcd.ErrorKeyNotFound}, nil, nil, true},
 		// bad JSON should cause errors
 		{nil, makeResponse(0, "asdf"), nil, true},
 		{nil, makeResponse(0, `{"semaphore:`), nil, true},
@@ -108,8 +109,8 @@ func TestEtcdLockClientSet(t *testing.T) {
 		{&Semaphore{}, nil, false},
 		{&Semaphore{Index: uint64(1234)}, nil, false},
 		// all errors returned from etcd should propagate
-		{&Semaphore{}, &etcd.Error{ErrorCode: etcd.ErrorNodeExist}, true},
-		{&Semaphore{}, &etcd.Error{ErrorCode: etcd.ErrorKeyNotFound}, true},
+		{&Semaphore{}, &goetcd.EtcdError{ErrorCode: etcd.ErrorNodeExist}, true},
+		{&Semaphore{}, &goetcd.EtcdError{ErrorCode: etcd.ErrorKeyNotFound}, true},
 		{&Semaphore{}, errors.New("some random error"), true},
 	} {
 		elc := &EtcdLockClient{